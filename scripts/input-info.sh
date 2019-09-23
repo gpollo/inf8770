1e364fcc1ee9ba3d5c510835c476df4fb5c15c6b
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-09-22 20:41:17 -0400

    Add basic README

---
+++ scripts/input-info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo ".Liste des Fichiers de Tests"
+echo "|==="
+echo "|Fichier ^|Taille (octets) ^|Entropie (bits/octet)"
+find . -type f -iname "*.decoded" | sort | while read FILE; do
+    DATA=$(ent -t "$FILE" | tail -n +2 | tr ',' ' ')
+    FILENAME=$(echo "$FILE" | sed 's/\.\///g')
+    SIZE=$(echo "$DATA" | awk '{print $2}')
+    ENTROPY=$(echo "$DATA" | awk '{print $3}')
+
+    echo ""
+    echo "| $FILENAME"
+    echo "^| $SIZE"
+    echo "^| $ENTROPY"
+done
+echo "|==="
