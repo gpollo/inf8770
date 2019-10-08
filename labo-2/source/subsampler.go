6c2c84ea76c88eb41fe9edfe1e386a99d59e1170
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-10-08 19:26:48 -0400

    Add 3 subsampling ratios

---
+++ labo-2/source/subsampler.go
@@ -0,0 +1,156 @@
+package main
+
+type subsampler interface {
+	Subsample(y, u, v ImageData) (ImageData, ImageData, ImageData)
+}
+
+type Subsampler410 struct{}
+
+func (s *Subsampler410) Subsample(y1, u1, v1 ImageData) (ImageData, ImageData, ImageData) {
+	ySizeX, ySizeY := y1.GetDimensions()
+	uSizeX, uSizeY := u1.GetDimensions()
+	vSizeX, vSizeY := v1.GetDimensions()
+
+	if ySizeX != uSizeX || ySizeX != vSizeX {
+		panic("X dimensions aren't equal")
+	}
+
+	if ySizeY != uSizeY || ySizeY != vSizeY {
+		panic("Y dimensions aren't equal")
+	}
+
+	sizeX := ySizeX
+	sizeY := ySizeY
+
+	y2 := NewImageData(sizeX, sizeY)
+	u2 := NewImageData(sizeX/4, sizeY/2)
+	v2 := NewImageData(sizeX/4, sizeY/2)
+
+	for j := 0; j < sizeY; j++ {
+		for i := 0; i < sizeX; i++ {
+			y2[j][i] = y1[j][i]
+		}
+	}
+
+	for j := 0; j < sizeY/2; j++ {
+		for i := 0; i < sizeX/4; i++ {
+			u := 0
+			u += int(u1[2*j+0][4*i+0])
+			u += int(u1[2*j+0][4*i+1])
+			u += int(u1[2*j+0][4*i+2])
+			u += int(u1[2*j+0][4*i+3])
+			u += int(u1[2*j+1][4*i+0])
+			u += int(u1[2*j+1][4*i+1])
+			u += int(u1[2*j+1][4*i+2])
+			u += int(u1[2*j+1][4*i+3])
+			u2[j][i] = byte(u / 8)
+
+			v := 0
+			v += int(v1[2*j+0][4*i+0])
+			v += int(v1[2*j+0][4*i+1])
+			v += int(v1[2*j+0][4*i+2])
+			v += int(v1[2*j+0][4*i+3])
+			v += int(v1[2*j+1][4*i+0])
+			v += int(v1[2*j+1][4*i+1])
+			v += int(v1[2*j+1][4*i+2])
+			v += int(v1[2*j+1][4*i+3])
+			v2[j][i] = byte(v / 8)
+		}
+	}
+
+	return y2, u2, v2
+}
+
+type Subsampler420 struct{}
+
+func (s *Subsampler420) Subsample(y1, u1, v1 ImageData) (ImageData, ImageData, ImageData) {
+	ySizeX, ySizeY := y1.GetDimensions()
+	uSizeX, uSizeY := u1.GetDimensions()
+	vSizeX, vSizeY := v1.GetDimensions()
+
+	if ySizeX != uSizeX || ySizeX != vSizeX {
+		panic("X dimensions aren't equal")
+	}
+
+	if ySizeY != uSizeY || ySizeY != vSizeY {
+		panic("Y dimensions aren't equal")
+	}
+
+	sizeX := ySizeX
+	sizeY := ySizeY
+
+	y2 := NewImageData(sizeX, sizeY)
+	u2 := NewImageData(sizeX/2, sizeY/2)
+	v2 := NewImageData(sizeX/2, sizeY/2)
+
+	for j := 0; j < sizeY; j++ {
+		for i := 0; i < sizeX; i++ {
+			y2[j][i] = y1[j][i]
+		}
+	}
+
+	for j := 0; j < sizeY/2; j++ {
+		for i := 0; i < sizeX/2; i++ {
+			u := 0
+			u += int(u1[2*j+0][2*i+0])
+			u += int(u1[2*j+1][2*i+0])
+			u += int(u1[2*j+1][2*i+1])
+			u += int(u1[2*j+0][2*i+1])
+			u2[j][i] = byte(u / 4)
+
+			v := 0
+			v += int(v1[2*j+0][2*i+0])
+			v += int(v1[2*j+1][2*i+0])
+			v += int(v1[2*j+1][2*i+1])
+			v += int(v1[2*j+0][2*i+1])
+			v2[j][i] = byte(v / 4)
+		}
+	}
+
+	return y2, u2, v2
+}
+
+type Subsampler422 struct{}
+
+func (s *Subsampler422) Subsample(y1, u1, v1 ImageData) (ImageData, ImageData, ImageData) {
+	ySizeX, ySizeY := y1.GetDimensions()
+	uSizeX, uSizeY := u1.GetDimensions()
+	vSizeX, vSizeY := v1.GetDimensions()
+
+	if ySizeX != uSizeX || ySizeX != vSizeX {
+		panic("X dimensions aren't equal")
+	}
+
+	if ySizeY != uSizeY || ySizeY != vSizeY {
+		panic("Y dimensions aren't equal")
+	}
+
+	sizeX := ySizeX
+	sizeY := ySizeY
+
+	y2 := NewImageData(sizeX, sizeY)
+	u2 := NewImageData(sizeX/2, sizeY)
+	v2 := NewImageData(sizeX/2, sizeY)
+
+	for j := 0; j < sizeY; j++ {
+		for i := 0; i < sizeX; i++ {
+			y2[j][i] = y1[j][i]
+		}
+	}
+
+	for j := 0; j < sizeY; j++ {
+		for i := 0; i < sizeX/2; i++ {
+			u := 0
+			u += int(u1[j][2*i+0])
+			u += int(u1[j][2*i+1])
+			u2[j][i] = byte(u / 2)
+
+			v := 0
+			v += int(v1[j][2*i+0])
+			v += int(v1[j][2*i+1])
+			v2[j][i] = byte(v / 2)
+		}
+	}
+
+	return y2, u2, v2
+}
---
+++ labo-2/source/subsampler_test.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"testing"
+)
+
+func assert2DByteArrayEqual(t *testing.T, got, expected [][]byte) {
+	if len(got) != len(expected) {
+		t.Fatalf("array size differs: got=%d, expected=%d", len(got), len(expected))
+	}
+
+	if len(got) == 0 {
+		return
+	}
+
+	if len(got[0]) != len(expected[0]) {
+		t.Fatalf("array size differs: got=%d, expected=%d", len(got[0]), len(expected[0]))
+	}
+
+	for j := 0; j < len(got); j++ {
+		for i := 0; i < len(got[0]); i++ {
+			if got[j][i] == expected[j][i] {
+				continue
+			}
+
+			t.Errorf("value at (%d, %d) differs: got=%d, expected=%d", i, j, got[j][i], expected[j][i])
+		}
+	}
+}
+
+func TestSubsampler410(t *testing.T) {
+	data := [][]byte{
+		{10, 10, 20, 20, 30, 30, 40, 50},
+		{30, 50, 10, 40, 80, 40, 20, 20},
+		{90, 90, 10, 60, 20, 60, 10, 70},
+		{10, 20, 30, 60, 10, 20, 30, 40},
+	}
+
+	yExpected := [][]byte{
+		{10, 10, 20, 20, 30, 30, 40, 50},
+		{30, 50, 10, 40, 80, 40, 20, 20},
+		{90, 90, 10, 60, 20, 60, 10, 70},
+		{10, 20, 30, 60, 10, 20, 30, 40},
+	}
+
+	uvExpected := [][]byte{
+		{23, 38},
+		{46, 32},
+	}
+
+	subsampler := Subsampler410{}
+	y, u, v := subsampler.Subsample(data, data, data)
+
+	assert2DByteArrayEqual(t, y, yExpected)
+	assert2DByteArrayEqual(t, u, uvExpected)
+	assert2DByteArrayEqual(t, v, uvExpected)
+}
+
+func TestSubsampler420(t *testing.T) {
+	data := [][]byte{
+		{10, 10, 20, 20, 30, 30, 40, 50},
+		{30, 50, 10, 40, 80, 40, 20, 20},
+		{90, 90, 10, 60, 20, 60, 10, 70},
+		{10, 20, 30, 60, 10, 20, 30, 40},
+	}
+
+	yExpected := [][]byte{
+		{10, 10, 20, 20, 30, 30, 40, 50},
+		{30, 50, 10, 40, 80, 40, 20, 20},
+		{90, 90, 10, 60, 20, 60, 10, 70},
+		{10, 20, 30, 60, 10, 20, 30, 40},
+	}
+
+	uvExpected := [][]byte{
+		{25, 22, 45, 32},
+		{52, 40, 27, 37},
+	}
+
+	subsampler := Subsampler420{}
+	y, u, v := subsampler.Subsample(data, data, data)
+
+	assert2DByteArrayEqual(t, y, yExpected)
+	assert2DByteArrayEqual(t, u, uvExpected)
+	assert2DByteArrayEqual(t, v, uvExpected)
+}
+
+func TestSubsampler422(t *testing.T) {
+	data := [][]byte{
+		{10, 10, 20, 20, 30, 30, 40, 50},
+		{30, 50, 10, 40, 80, 40, 20, 20},
+		{90, 90, 10, 60, 20, 60, 10, 70},
+		{10, 20, 30, 60, 10, 20, 30, 40},
+	}
+
+	yExpected := [][]byte{
+		{10, 10, 20, 20, 30, 30, 40, 50},
+		{30, 50, 10, 40, 80, 40, 20, 20},
+		{90, 90, 10, 60, 20, 60, 10, 70},
+		{10, 20, 30, 60, 10, 20, 30, 40},
+	}
+
+	uvExpected := [][]byte{
+		{10, 20, 30, 45},
+		{40, 25, 60, 20},
+		{90, 35, 40, 40},
+		{15, 45, 15, 35},
+	}
+
+	subsampler := Subsampler422{}
+	y, u, v := subsampler.Subsample(data, data, data)
+
+	assert2DByteArrayEqual(t, y, yExpected)
+	assert2DByteArrayEqual(t, u, uvExpected)
+	assert2DByteArrayEqual(t, v, uvExpected)
+}
