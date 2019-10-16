122b1230a5fcc309a40ef20bb662f090ee7a60c1
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-10-15 21:39:41 -0400

    Add an helper function for saving an image

--- labo-2/jpeg2000/main/helper.go
+++ labo-2/jpeg2000/main/helper.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"encoding/binary"
+	"errors"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"os"
 	"os/exec"
+	"strings"
+
+	"golang.org/x/image/bmp"
 )
 
 func ReadAll(r io.Reader) ([]byte, error) {
@@ -67,6 +74,34 @@ func WriteVarint(w io.Writer, x int64) error {
 
 	if _, err := w.Write(buffer[0:count]); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func SaveImage(i image.Image, f *os.File) error {
+	splited := strings.Split(f.Name(), ".")
+	if len(splited) <= 1 {
+		return errors.New("Extension not found in output filename")
+	}
+
+	extension := splited[len(splited)-1]
+
+	switch extension {
+	case "bmp":
+		if err := bmp.Encode(f, i); err != nil {
+			return err
+		}
+	case "jpg":
+		fallthrough
+	case "jpeg":
+		if err := jpeg.Encode(f, i, nil); err != nil {
+			return err
+		}
+	case "png":
+		if err := png.Encode(f, i); err != nil {
+			return err
+		}
 	}
 
 	return nil
