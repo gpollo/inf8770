f29f5073c41448878b0a4d3690585622225660bf
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-10-18 14:07:39 -0400

    Add various image tests

---
+++ labo-2/scripts/table-images.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function output_header() {
+    echo "// auto-generated table, do not edit"
+    echo ".$TABLE_TITLE"
+    echo "[%autowidth,cols=4,frame=none,grid=none,stripe=all]"
+    echo "|==="
+}
+
+function output_table() {
+    local files=${*}
+    local filename
+    local queue
+
+    # TODO: that's not gonna work with image count different than a multiple of 4
+
+    queue=()
+    for encoded in ${files[*]}; do
+        if echo "$encoded" | grep scripts > /dev/null; then
+            continue
+        fi
+
+        filename=${encoded//various-/}
+        filename=${filename//png/bmp}
+        echo "^| \`$(basename "$filename")\`"
+
+        queue+=($encoded)
+        if [[ "${#queue[@]}" == "4" ]]; then
+            for image in ${queue[*]}; do
+                echo "^|image:${image}[width=140]"
+            done
+            queue=()
+        fi
+    done
+
+}
+
+function output_footer() {
+    echo "|==="
+}
+
+output_header
+output_table "${*}"
+output_footer
